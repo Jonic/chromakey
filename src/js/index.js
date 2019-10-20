444ed34cacbfc94b4f642683ccd6354d9b745e0f
Jonic Linley <jonic@100yen.co.uk>
2019-10-20 21:35:07 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,84 @@
+import * as dat from 'dat.gui'
+
+const lerp = (start, end, t) => start * (1 - t) + end * t
+
 class Application {
   constructor() {
-    this.body = document.body
-    this.initKeyboardEvents()
+    this.colorKeys = '1qaz2wsx3edc4rfv5tgb6yhn7ujm8ik,9ol.0p'.split('')
+    this.endColor = [1, 64, 200]
+    this.history = []
+    this.shape = 'circle'
+    this.size = 30
+    this.spacing = 25
+    this.startColor = [200, 64, 1]
 
-    this.specialKeycodes = ['Backspace', 'Space']
+    this.currentPosition = {
+      x: 0,
+      y: 0,
+    }
+
+    this.initCanvas()
+    this.initKeyboardEvents()
+    // this.initDatGui()
+  }
+
+  initCanvas() {
+    this.canvas = document.querySelector('.c-canvas')
+    this.context = this.canvas.getContext('2d')
+
+    this.canvas.width = document.body.clientWidth
+    this.canvas.height = document.body.clientHeight
+
+    const devicePixelRatio = window.devicePixelRatio || 1
+    const backingStoreRatio =
+      this.context.webkitBackingStorePixelRatio ||
+      this.context.backingStorePixelRatio ||
+      1
+    const ratio = devicePixelRatio / backingStoreRatio
+
+    if (devicePixelRatio !== backingStoreRatio) {
+      const oldWidth = this.canvas.width
+      const oldHeight = this.canvas.height
+
+      this.canvas.width = oldWidth * ratio
+      this.canvas.height = oldHeight * ratio
+
+      this.canvas.style.width = oldWidth + 'px'
+      this.canvas.style.height = oldHeight + 'px'
+
+      this.context.scale(ratio, ratio)
+    }
+  }
+
+  initDatGui() {
+    const gui = new dat.GUI()
+
+    gui.add(this, 'size', 1, 100)
+    gui.add(this, 'spacing', 1, 100)
+    gui.add(this, 'shape', ['circle', 'triangle', 'hexagon', 'square'])
+    gui.addColor(this, 'startColor')
+    gui.addColor(this, 'endColor')
+
+    gui.remember(this)
   }
 
   initKeyboardEvents() {
-    const keyRegex = RegExp('^(Digit[0-9]{1})|(Key[A-Z]{1})$')
-
-    this.body.addEventListener('keydown', event => {
+    window.addEventListener('keydown', event => {
       const { code, key } = event
+      console.log(this.colorKeys.indexOf(key))
 
-      if (keyRegex.test(code)) {
+      if (this.colorKeys.indexOf(key) > -1) {
         return this.applyKey(key)
       }
 
-      if (code === 'Space') {
-        return this.applySpace()
+      if (code === 'Enter') {
+        this.currentPosition.x = 0
+        this.currentPosition.y += this.spacing
+        return
+      }
+
+      if (code === 'Space' || key === '+') {
+        return this.applyKey('+')
       }
 
       if (code === 'Backspace') {
@@ -29,15 +90,49 @@ class Application {
   }
 
   applyBackspace() {
-    console.log('Applying Backspace')
+    const poppedKey = this.history.pop()
+    console.log(this.history.join(''))
   }
 
   applyKey(key) {
-    console.log('Applying Key:', key)
-  }
+    this.history.push(key)
+    console.log(this.history.join(''))
 
-  applySpace() {
-    console.log('Applying Space')
+    if (key !== '+') {
+      const arrayOffset = this.colorKeys.indexOf(key)
+      const t = arrayOffset / this.colorKeys.length
+      const color = [
+        lerp(this.startColor[0], this.endColor[0], t),
+        lerp(this.startColor[1], this.endColor[1], t),
+        lerp(this.startColor[2], this.endColor[2], t),
+      ]
+
+      // console.table({
+      //   arrayOffset,
+      //   t,
+      //   color,
+      // })
+
+      this.context.fillStyle = `rgba(${color.join(', ')}, 0.85)`
+      this.context.beginPath()
+      this.context.arc(
+        this.currentPosition.x,
+        this.currentPosition.y,
+        this.size / 2,
+        0,
+        2 * Math.PI,
+        true
+      )
+      this.context.closePath()
+      this.context.fill()
+    }
+
+    this.currentPosition.x += this.spacing
+
+    if (this.currentPosition.x > document.body.clientWidth) {
+      this.currentPosition.x = 0
+      this.currentPosition.y += this.spacing
+    }
   }
 }
 
